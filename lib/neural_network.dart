32810ee089754c26895335acc60879dd6ac7a281
Mohamed Lotfy <molotfy50@gmail.com>
2022-08-03 19:23:51 +0200

    add activation to nn

--- lib/neural_network.dart
+++ lib/neural_network.dart
@@ -1,1 +1,5 @@
 export './src/nn/dense_layer.dart';
+export './src/activation/relu.dart';
+export './src/activation/sigmoid.dart';
+export './src/activation/softmax.dart';
+export './src/activation/relu.dart';
